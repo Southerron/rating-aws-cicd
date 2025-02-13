9c76761139c447746f7c6b0003a3e83082d1b614
Laihao Yang <la.yang@reply.it>
2025-02-13 15:49:24 +0100

    aggiunta timer

--- src/main/java/com/rating/controller/RatingController.java
+++ src/main/java/com/rating/controller/RatingController.java
@@ -3,6 +3,8 @@ package com.rating.controller;
 import com.rating.model.ProfessionDomainList;
 import com.rating.service.JsonReaderService;
 import com.rating.service.RatingDomainService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,24 @@ public class RatingController {
     @Autowired
     private JsonReaderService jsonReaderService;
 
+    private static final Logger logger = LoggerFactory.getLogger(RatingController.class);
+
 
     @GetMapping(produces = "application/json", value = "/professions")
+
     public ResponseEntity<ProfessionDomainList> getProfessionDomain() {
+        long startTime = System.currentTimeMillis();
+
+        logger.info("Lambda function started at: " + startTime);
+
         ProfessionDomainList professionDomainList =  ratingDomainService.getProfessionFromJson();
 
+        long endTime = System.currentTimeMillis();
+        logger.info("Lambda function ended at: " + endTime);
+        logger.info("Total execution time: " + (endTime - startTime) + " ms");
+
         return new ResponseEntity<>(professionDomainList, HttpStatus.OK);
+
     }
 
 }
